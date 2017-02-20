8478fb48ce0afa12c56d24d95892f5b94911519d
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2017-02-20 10:07:49 +0100

    Creando las clases

---
+++ src/cdmaquinacafe/Maquina.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdmaquinacafe;
+
+/**
+ *
+ * @author otorradomiguez
+ */
+public class Maquina {
+    //el tipo de producto se refiere al boton que se pulse
+    //si se kiere cambiar hay k modificar precio en monedero
+    int stockP1,stockP2,stockP3;
+    int azucar=5;
+    int producto;
+    boolean preparando=false;
+    
+}
---
+++ src/cdmaquinacafe/Monedero.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdmaquinacafe;
+
+/**
+ *
+ * @author otorradomiguez
+ */
+public class Monedero {
+    //llevaremos la cuenta en centimos
+    int saldo;
+    int precioP1=50,precioP2=60,precioP3=80;
+}
